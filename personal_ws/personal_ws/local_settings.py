17487b1175e202ca82b24847e44039256b0981ca
lucasbrambrink <lbrambrink@gmail.com>
2015-04-13 00:05:23 -0400

    moved images to proper directories and set up main css files

--- personal_ws/personal_ws/local_settings.py
+++ personal_ws/personal_ws/local_settings.py
@@ -2,3 +2,14 @@ SECRET_KEY = '+-nn!703*mdznj)o*doue7d9)^nmu1367bl3yc3^y&&esrowla'
 
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'personal_ws',
+        'USER': 'lb',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+        }
+}
