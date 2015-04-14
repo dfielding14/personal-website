534ec97c36de27c5eaa411b6defdcf675ae8692a
lucasbrambrink <lbrambrink@gmail.com>
2015-04-14 15:13:54 -0400

    added apache settings config (needs work) and keyboard arrow functionality

--- personal_ws/main/static/js/main.js
+++ personal_ws/main/static/js/main.js
@@ -5,4 +5,15 @@ $(document).ready(function() {
     // initializing page transition.
     PageTransitions.init();
 
+    $("body").keydown(function(e) {
+           if(e.keyCode == 37) { // left
+                $(".left").trigger("click");
+           } else if(e.keyCode == 39) { // right
+                $(".right").trigger("click");
+           } else if(e.keyCode == 40) { // down
+                $(".down").trigger("click");
+           } else if(e.keyCode == 38) { // top
+                $(".top").trigger("click")
+           }
+      });
 });
